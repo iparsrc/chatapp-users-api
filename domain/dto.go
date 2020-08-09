8ff2fc1413b006c4606b16bf16825d77eee0a9cc
parsa <akbariparsa1209@gmail.com>
2020-08-09 17:40:55 +0430

    Created domain package and defined User type.

---
+++ domain/dto.go
@@ -0,0 +1,14 @@
+package domain
+
+type User struct {
+	// These fields are provided from google.
+	ID         string `json:"id" bson:"_id"`
+	Email      string `json:"email" bson:"email"`
+	Picture    string `json:"picture" bson:"picture"`
+	FullName   string `json:"fullName" bson:"fullName"`
+	GivenName  string `json:"givenName" bson:"givenName"`
+	FamilyName string `json:"familyName" bson:"familyName"`
+	// These fields are platform specific.
+	ContactsIDs    []string `json:"contactsIds" bson:"contactsIds"`
+	JoinedGroupIDs []string `json:"joinedGroupIds" bson:"joinedGroupIds"`
+}
